6bab6b77bf6767386d4e032abf54556169b96432
FelipeECass <fel.evancass@gmail.com>
2021-04-12 10:18:25 -0300

    Add project files.

---
+++ ProjetoCadastrodeSeriesDIO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetoCadastrodeSeriesDIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
